1c04e876e1a59b3623fec28c6ea641c695a48956
枫叶 <1464435902@qq.com>
2021-11-16 19:23:19 +0800

    first commit3

--- app/src/main/java/org/fengye/commonview/MainActivity.kt
+++ app/src/main/java/org/fengye/commonview/MainActivity.kt
@@ -2,11 +2,17 @@ package org.fengye.commonview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.fengye.commonview.databinding.ActivityMainBinding
+import org.fengye.commonview.lib.scrollpicker.DatePickerAdapter
+import org.fengye.commonview.lib.scrollpicker.PickAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.yearPicker.setAdapter(DatePickerAdapter(1, 10))
     }
 }
